cac83f37694e03f017716321f8b146fa10fce087
JosephPR <reilly4181@gmail.com>
2019-08-26 18:01:18 -0600

    refactor

--- src/components/exercise-list.jsx
+++ src/components/exercise-list.jsx
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from  'react-router-dom';
 import axios from 'axios';
 
-const Exercise = props => (
-  <tr>
-    <td>{props.exercise.username}</td>
-    <td>{props.exercise.description}</td>
-    <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0,10)}</td>
-    <td>
-      <Link to={`/edit/${props.exercise._id}`}>edit</Link> | <a href="#" onClick={() => {props.deleteExercise(props.exercise._id) }}>Delete</a>
-    </td>
-  </tr>
-)
+import Exercise from './exercise'
+
 
 export default class ExerciseList extends Component {
   constructor (props) {
