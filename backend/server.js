72d36bffd4577c04c07650fd00cc4c4d16098266
Josephpr <reilly4181@gmail.com>
2024-03-24 18:19:31 -0600

    get running again

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database successful connection");
